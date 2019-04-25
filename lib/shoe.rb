7415288cb0583dd62e3037d92d68fcec8f7820fa
Katherine Wren <kwren@outlook.com>
2019-04-25 10:03:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -7,6 +8,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     @@BRANDS << self.brand
+    binding.pry
   end
   
   def cobble
